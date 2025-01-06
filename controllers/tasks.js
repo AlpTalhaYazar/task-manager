a6910da13676d9feafea3e5a5f03c94de3e1c92a
Alp Talha Yazar <AlpTalhaYazar@gmail.com>
2025-01-06 17:30:19 +0300

    feat: implement CRUD operations for Task management in controller

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,21 +1,96 @@
-const getAllTasks = (req, res) => {
-    res.send('All Tasks');
+const Task = require('../models/Task');
+
+const getAllTasks = async (req, res) => {
+    try {
+        var tasks = await Task.find({});
+        res.json(tasks);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 };
 
-const createTask = (req, res) => {
-    res.send('Create Task');
+const createTask = async (req, res) => {
+    try {
+        const task = new Task(req.body);
+
+        var createdTask = await task.save();
+
+        res.status(201).json(createdTask);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 };
 
-const getTask = (req, res) => {
-    res.send('Get Task with id ' + req.params.id);
+const getTask = async (req, res) => {
+    try {
+        var task = await Task.findById(req.params.id);
+
+        if (!task) {
+            return res.status(404).send({
+                message: "Task not found with id " + req.params.id
+            });
+        }
+
+        res.json(task);
+    } catch (error) {
+        if (error.kind === 'ObjectId') {
+            return res.status(404).send({
+                message: "Task not found with id " + req.params.id
+            });
+        }
+
+        return res.status(500).send({
+            message: "Error retrieving task with id " + req.params.id
+        });
+    }
 }
 
-const updateTask = (req, res) => {
-    res.send('Update Task with id ' + req.params.id);
+const updateTask = async (req, res) => {
+    try {
+        var task = await Task.findByIdAndUpdate(req.params.id);
+
+        if (!task) {
+            return res.status(404).send({
+                message: "Task not found with id " + req.params.id
+            });
+        }
+
+        res.json(task);
+    } catch (error) {
+        if (error.kind === 'ObjectId') {
+            return res.status(404).send({
+                message: "Task not found with id " + req.params.id
+            });
+        }
+
+        return res.status(500).send({
+            message: "Error updating task with id " + req.params.id
+        });
+    }
 }
 
-const deleteTask = (req, res) => {
-    res.send('Delete Task with id ' + req.params.id);
+const deleteTask = async (req, res) => {
+    try {
+        var task = await Task.findByIdAndRemove(req.params.id);
+
+        if (!task) {
+            return res.status(404).send({
+                message: "Task not found with id " + req.params.id
+            });
+        }
+
+        res.json({ message: "Task deleted successfully!" });
+    } catch (error) {
+        if (error.kind === 'ObjectId' || error.name === 'NotFound') {
+            return res.status(404).send({
+                message: "Task not found with id " + req.params.id
+            });
+        }
+
+        return res.status(500).send({
+            message: "Could not delete task with id " + req.params.id
+        });
+    }
 }
 
 module.exports = {
