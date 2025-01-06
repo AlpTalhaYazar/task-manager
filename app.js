1220254a48d1297be75bca5c11409974192d879f
Alp Talha Yazar <AlpTalhaYazar@gmail.com>
2025-01-06 16:19:13 +0300

    feat: integrate MongoDB connection and update server initialization

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
+const connectDB = require('./db/connect');
 
 const taskRoutes = require('./routes/tasks');
 
@@ -9,6 +11,18 @@ app.use(express.json());
 
 app.use('/api/v1/tasks', taskRoutes);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.listen(port, console.log(`Server is running on port ${port}`));
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+            .then(() => console.log('Connected to the database'))
+            .catch((error) => console.error(error));
+
+        app.listen(port, console.log(`Server is running on port ${port}`));
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+start();
