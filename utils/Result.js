7bfb822011d9f52997ae7d27bea73ed0288424a8
Alp Talha Yazar <AlpTalhaYazar@gmail.com>
2025-01-07 12:58:28 +0300

    feat: add Result class for standardized success and failure responses

---
+++ utils/Result.js
@@ -0,0 +1,17 @@
+class Result {
+    constructor(isSuccess, data, error) {
+        this.isSuccess = isSuccess;
+        this.data = data;
+        this.error = error;
+    }
+
+    static success(data) {
+        return new Result(true, data, null);
+    }
+
+    static failure(error) {
+        return new Result(false, null, error);
+    }
+}
+
+module.exports = Result;
