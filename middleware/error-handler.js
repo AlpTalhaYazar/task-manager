5d80dd40a663ffecfac600797d88dae887cd08af
Alp Talha Yazar <AlpTalhaYazar@gmail.com>
2025-01-07 15:14:30 +0300

    fix: update error handler to use statusCode for consistent error responses

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -4,7 +4,7 @@ const Result = require('../utils/Result');
 const errorHandler = (err, req, res, next) => {
     let result = Result.failure(err.message);
 
-    res.status(err.status || StatusCodes.INTERNAL_SERVER_ERROR)
+    res.status(err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR)
         .json(result);
 }
 
