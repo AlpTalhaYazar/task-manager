a0cbe95919a09a3aa22c9ea5b28934d621ab7f19
Alp Talha Yazar <AlpTalhaYazar@gmail.com>
2025-01-09 21:05:26 +0300

    feat: add parseQuery middleware to enhance task retrieval queries

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,9 +1,10 @@
 import express from 'express';
+import { parseQuery } from '../middleware/parse-query.js';
 import { getAllTasks, createTask, getTask, updateTask, deleteTask } from '../controllers/tasks.js';
 
 const router = express.Router();
 
-router.route('/').get(getAllTasks).post(createTask);
+router.route('/').get(parseQuery, getAllTasks).post(createTask);
 router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask);
 
 export { router as taskRoutes };
